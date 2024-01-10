fdbe7e90150e4a3768c1e4252347e5eb57037d3a
chriamue <chriamue@users.noreply.github.com>
2024-01-10 17:22:09 +0000

    deploy: 55e5d2cefe24b19f55e0ddb6e3ffdd62c0cf030f

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = JSON.parse('{\
-"chat_flame_backend":["",[["api",[["model",[],["mod.rs"]],["routes",[],["generate.rs","generate_stream.rs","generate_text.rs","health.rs","info.rs","mod.rs"]]],["mod.rs","openapi.rs"]],["llm",[["models",[],["mod.rs"]],["text_generator",[],["dummy_text_generator.rs","mod.rs"]],["token_generator",[],["dummy.rs","mod.rs"]]],["generate_parameter.rs","loader.rs","mod.rs","model_processor.rs","sampler.rs","text_generation.rs"]]],["config.rs","lib.rs","server.rs"]]\
+"chat_flame_backend":["",[["api",[["model",[],["mod.rs"]],["routes",[],["generate.rs","generate_stream.rs","generate_text.rs","health.rs","info.rs","mod.rs","model.rs"]]],["mod.rs","openapi.rs"]],["llm",[["models",[],["mod.rs"]],["text_generator",[],["dummy_text_generator.rs","mod.rs"]],["token_generator",[],["dummy.rs","mod.rs"]]],["generate_parameter.rs","loader.rs","mod.rs","model_processor.rs","sampler.rs","text_generation.rs"]]],["config.rs","lib.rs","server.rs"]]\
 }');
 createSrcSidebar();
